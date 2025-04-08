64017a7f0a049c847117854c435fe2886b533bc7
JMRothdach1 <justusrothdach@googlemail.com>
2025-04-08 11:13:59 +0200

    fix

--- LandingPage.py
+++ LandingPage.py
@@ -4,15 +4,16 @@ def landing_page():
     """Kommentar für Funktionen so formatieren. Dadurch kann das hier geschriebene
     auch mit help(laning_page) abgerufen werden.
     """
-    st.title("Job Fit Application")
-    st.subheader("Welcome to your personal Job Matcher")
-    st.write("Find the best job for you based on your preferences!")
-    st.write("Please chose your prefered function on the left sidebar:")
+    
+    
+    
+st.title("Job Fit Application")
+st.subheader("Welcome to your personal Job Matcher")
+st.write("Find the best job for you based on your preferences!")
+st.write("Please chose your prefered function on the left sidebar:")
 
-    menü = st.sidebar.radio("Menu", ("Startseite", "Job Matcher", "Job nach Region", "Gehaltsfinder", "About" ))
+menü = st.sidebar.radio("Menu", ("Startseite", "Job Matcher", "Job nach Region", "Gehaltsfinder", "About" ))
     if menü == "Startseite": 
         st.write("This is the Startseite page")
 
     if menü == "Job Matcher":
-
-landing_page
