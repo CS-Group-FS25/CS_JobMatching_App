fae9bbf1518c01d17631875c443d4d4ce668d609
JMRothdach1 <justusrothdach@googlemail.com>
2025-04-08 15:12:44 +0200

    fixing

--- LandingPage.py
+++ LandingPage.py
@@ -13,9 +13,10 @@ st.write("Find the best job for you based on your preferences!")
 st.write("Please chose your prefered function on the left sidebar:")
 
 menü = st.sidebar.radio("Menu", ("Startseite", "Job Matcher", "Job nach Region", "Gehaltsfinder", "About" ))
-    if menü == "Startseite": 
-        st.write("This is the Startseite page")
 
-    if menü == "Job Matcher":
+if menü == "Startseite": 
+    st.write("This is the Startseite page")
+
+if menü == "Job Matcher":
     
         
