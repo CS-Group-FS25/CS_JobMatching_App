d7885d5c1235deea2a5057851465f8e84d03f8d2
NicoHSG <nicolas.schmid.schulmail@gmail.com>
2025-04-17 14:12:24 +0500

    weitere Änderungen

--- LandingPage.py
+++ LandingPage.py
@@ -1,15 +1,16 @@
 import streamlit as st
 
 # Titel und Beschreibung
-st.title("Job Matching Application")
-st.subheader("Willkommen zu unserem Job matcher!")
-st.write("Finde mit 3 einfachen Schritten zu deinem Traumjob!")
+
 
 # Sidebar-Menü
 menu = st.sidebar.radio("Menu", ("Startseite", "Job Matcher", "Job nach Region", "Gehaltsfinder", "About"))
 
 # Inhalte auf der Startseite
 if menu == "Startseite":
+    st.title("Job Matching Application")
+    st.subheader("Willkommen zu unserem Job matcher!")
+    st.write("Finde mit 3 einfachen Schritten zu deinem Traumjob!")
     st.write("Finde jetzt deinen Traumjob!")
     st.subheader("Schritt 1 - zeige uns deine Interessen")
     st.subheader("Schritt 2 - Zeige uns deine Stärken")
