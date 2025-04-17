51a36d429bf56befe74300529ca61866aaa1019d
NicoHSG <nicolas.schmid.schulmail@gmail.com>
2025-04-17 13:35:24 +0500

    erste Änderungen von Justus LP kopiert

--- LandingPage.py
+++ LandingPage.py
@@ -2,6 +2,19 @@ import streamlit as st
 
 def landing_page():
     """Kommentar für Funktionen so formatieren. Dadurch kann das hier geschriebene
-    auch mit help(laning_page) abgerufen werden.
+    auch mit help(landing_page) abgerufen werden.
     """
     st.write("THIS IS THE LANDING PAGE")
+
+    import streamlit as st
+
+st.write("This is the landing page")
+
+    
+    
+st.title("Job Fit Application")
+st.subheader("Welcome to your personal Job Matcher")
+st.write("Find the best job for you based on your preferences!")
+st.write("Please chose your prefered function on the left sidebar:")
+
+menü = st.sidebar.radio("Menu", ("Startseite", "Job Matcher", "Job nach Region", "Gehaltsfinder", "About" ))
