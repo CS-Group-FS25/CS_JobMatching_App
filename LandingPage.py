6bff0d78273b4b70d998c3b9fc43ad64fdb7dbaf
NicoHSG <nicolas.schmid.schulmail@gmail.com>
2025-04-17 14:13:27 +0500

    Teständerung

--- LandingPage.py
+++ LandingPage.py
@@ -11,7 +11,6 @@ if menu == "Startseite":
     st.title("Job Matching Application")
     st.subheader("Willkommen zu unserem Job matcher!")
     st.write("Finde mit 3 einfachen Schritten zu deinem Traumjob!")
-    st.write("Finde jetzt deinen Traumjob!")
     st.subheader("Schritt 1 - zeige uns deine Interessen")
     st.subheader("Schritt 2 - Zeige uns deine Stärken")
     st.subheader("Schritt 3 - Finde den Job, der zu dir passt!")
