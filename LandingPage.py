dedbda51c8faea0cfee7b52d6e887de87cb56985
JMRothdach1 <justusrothdach@googlemail.com>
2025-04-18 14:10:54 +0200

    test

--- LandingPage.py
+++ LandingPage.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+
+
 st.write("This is the landing page")
 
     
@@ -10,3 +13,14 @@ st.write("Please chose your prefered function on the left sidebar:")
 
 menü = st.sidebar.radio("Menu", ("Startseite", "Job Matcher", "Job nach Region", "Gehaltsfinder", "About" ))
 
+if menü == "Startseite":
+    st.write("Du bist auf der Startseite")
+elif menü == "Job Matcher":
+    import PageOne
+    PageOne.main()
+elif menü == "Job nach Region":
+    import PageTwo
+    PageTwo.main()
+elif menü == "Gehaltsfinder":
+    import PageThree
+    PageThree.main()
