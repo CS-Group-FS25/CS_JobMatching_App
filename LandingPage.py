626473e81317c38fa6af03f5039ea52110151f73
JMRothdach1 <justusrothdach@googlemail.com>
2025-04-18 14:16:52 +0200

    fix

--- LandingPage.py
+++ LandingPage.py
@@ -4,23 +4,3 @@ import pandas as pd
 
 st.write("This is the landing page")
 
-    
-    
-st.title("Job Fit Application")
-st.subheader("Welcome to your personal Job Matcher")
-st.write("Find the best job for you based on your preferences!")
-st.write("Please chose your prefered function on the left sidebar:")
-
-menü = st.sidebar.radio("Menu", ("Startseite", "Job Matcher", "Job nach Region", "Gehaltsfinder", "About" ))
-
-if menü == "Startseite":
-    st.write("Du bist auf der Startseite")
-elif menü == "Job Matcher":
-    import PageOne
-    PageOne.main()
-elif menü == "Job nach Region":
-    import PageTwo
-    PageTwo.main()
-elif menü == "Gehaltsfinder":
-    import PageThree
-    PageThree.main()
