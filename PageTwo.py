bac07d60e9394005a3a304337e223d2435143f2b
DRhsg <dominik.rueck@gmx.net>
2025-04-19 10:41:07 +0200

    Test und UI jobs in der region

--- PageTwo.py
+++ PageTwo.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
+import pydeck as pdk
 
 def page_two():
-    st.write("PAGE TWO")
+    st.write("Diese Jobs gibt es in deiner Region")
+
+# Streamlit UI
+st.title("Aktuelle Jobs in deiner Nähe")
+
+address = st.text_input("Ort eingeben", "Zürich")
+radius = st.slider("Umkreis in km", 5, 10, 25, 50, 100)
+
