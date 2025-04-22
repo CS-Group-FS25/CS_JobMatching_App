0fb2cd7f0eba5e03042c6c39b29a64318c47f6cb
Jake <jaksch03@gmail.com>
2025-04-22 17:38:12 +0200

    Data Handling Implementation

--- data_handling.py
+++ data_handling.py
@@ -5,15 +5,23 @@ from collections import Counter
 skills_df = pd.read_csv("job_skills.csv", usecols=["job_link", "job_skills"])
 jobs_df = pd.read_csv("linkedin_job_postings.csv", usecols=["job_link", "job_title", "search_position"])
 
+
 uncleaned_title = [] # only for testing - delete later!
 
 def extract_job_core(title):
     separators = r"[-–•|/,.\s]{2,}"
-    title = title.lower()
+    title = str(title).lower()
     parts = re.split(separators, title)
     parts = [p.strip() for p in parts if p.strip()]
 
-    job_keywords = [
+    for part in parts:
+        for keyword in job_keywords:
+            if keyword in part:
+                return part.title()
+    uncleaned_title.append(title)
+    return title.title()    # Default return when no keyword is found
+
+job_keywords = [
         "engineer", "developer", "manager", "designer", "consultant", "analyst",
         "specialist", "technician", "coordinator", "director", "officer",
         "scientist", "assistant", "architect", "administrator", "nurse", "teacher",
@@ -30,13 +38,6 @@ def extract_job_core(title):
         "physiologist", "landscaper", "receptionist", "head", "maker", "videographer", "professor", "mentor",
         "auditor", "examiner", "principal",
     ]
-
-    for part in parts:
-        for keyword in job_keywords:
-            if keyword in part:
-                return part.title()
-    uncleaned_title.append(title)
-    return title.title()    # Default return when no keyword is found
 
 ### clear list of empty entries and duplicates
 skills_df.dropna()
@@ -62,37 +63,17 @@ for title in uncleaned_title:
 
 print(word_counter.most_common(50))
 
-job_keywords = [
-        "engineer", "developer", "manager", "designer", "consultant", "analyst",
-        "specialist", "technician", "coordinator", "director", "officer",
-        "scientist", "assistant", "architect", "administrator", "nurse", "teacher",
-        "sales", "marketing", "intern", "writer", "accountant", "editor", "executive",
-        "supervisor", "counselor", "model", "physician", "physicist", "psychologist", "trainer", "professional",
-        "associate", "instructor", "producer", "therapist", "clerk", "chef", "surveyor", "advisor", "adviser",
-        "host", "technologist", "researcher", "agent", "leader", "finance", "provider", "ambassador",
-        "maintenance", "generalist", "representative", "tester", "superintendent", "lead", "senior", "volunteer",
-        "inspector", "attorney", "cook", "general", "veterinarian", "dentist", "advocate", "cashier", "worker",
-        "attendant", "medical", "coach", "guest", "operator", "psychiatrist", "foreman", "emergency", "clinical",
-        "service", "planner", "stylist", "staff", "surgeon", "management", "controller", "pediatric", "hospitalist",
-        "estimator", "tutor", "buyer", "private", "mechanic", "holder", "practitioner", "banker", "conductor",
-        "housekeeper", "driver", "recruiter", "oncology", "partner", "chemist", "server", "lecturer",
-        "physiologist", "landscaper", "receptionist", "head", "maker", "videographer", "professor", "mentor",
-        "auditor", "examiner", "principal",
-    ]
-
 jobs_df["matched"] = jobs_df["job_title_cleaned"].str.lower().apply(
     lambda t: any(keyword in t for keyword in job_keywords)
 )
 
-jobs_df[jobs_df["matched"]].to_csv("jobtitles_cleaned_matched.csv", index=False)
-
-jobs_df[~jobs_df["matched"]].to_csv("jobtitles_cleaned_unmatched.csv", index=False)
+# jobs_df[jobs_df["matched"]].to_csv("jobtitles_cleaned_matched.csv", index=False)
+# jobs_df[~jobs_df["matched"]].to_csv("jobtitles_cleaned_unmatched.csv", index=False)
 
 ### merge both jobs and skills list by matching job link
 matched_jobs_skills_df = pd.merge(jobs_df, skills_df, on='job_link', how='inner')
-
-print(matched_jobs_skills_df[["job_title_cleaned", "job_skills", "search_position"]].head())
-print(matched_jobs_skills_df.shape)
+# matched_jobs_skills_df.to_csv("matched_jobs_skills.csv", index=False)
+print("final shape:", matched_jobs_skills_df.shape)
 
 
 
