ae1d4cb23b947c2331a1bd29dfbc7dac16bd7e90
NicoHSG <nicolas.schmid.schulmail@gmail.com>
2025-04-28 11:42:59 +0500

    zwischenspeichern

--- LandingPage.py
+++ LandingPage.py
@@ -41,8 +41,7 @@ if st.session_state.seite == "Startseite":
 # Job Matcher-Inhalte
 elif st.session_state.seite == "Job Matcher":
     st.title("Willkommen beim Job Matcher")
-    import PageOne.py 
-    api_google_jobs()
+   
     
     # Button selbsterklären
     if st.button("Zurück zur Startseite"):
