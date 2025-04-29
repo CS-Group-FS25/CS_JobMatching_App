20772aff5a291807ce35c0d541c700cbb51fef4e
JMRothdach1 <justusrothdach@googlemail.com>
2025-04-29 16:07:06 +0200

    fix

--- PageOne.py
+++ PageOne.py
@@ -31,7 +31,7 @@ def datenabfrage():
     Ort = st.text_input("In welcher Region suchst du nach einem Job?")
     Branche = st.multiselect("Welche Branche interessiert dich?(Mehrfachauswahl möglich)", ["Finanzen", "Software", "Vertrieb","Beratung" "Soziales", "Lehrer", "Baubranche", "Verwaltung"
                                                                        "Logistik", "Handel", "Industrie", "Pharma", "Dienstleistungen"])
-    Bildungsabschluss = st.radio("Hast du einen Bildungsabschluss?", ("Keine Angabe","Ja", "Nein"))
+    Bildungsabschluss = st.radio("Hast du einen Bildungsabschluss?", ("Ja", "Nein"))
     if Bildungsabschluss == "Ja":
         Akademisches_Niveau = st.radio("Welche Ausbildung haben Sie?", ("Schulabschluss", "Ausbildung", "Studium", ))    
     Berufserfahrung = st.selectbox("Wie viel Berufserfahrung haben Sie?", ("Keine Erfahrung", "0-1 Jahr", "2-5 Jahre", "Mehr als 5 Jahre"))
