ed74f738cd7020d43e2ef423c98822a495e9c03d
JMRothdach1 <justusrothdach@googlemail.com>
2025-05-01 13:23:38 +0200

    try

--- PageOne.py
+++ PageOne.py
@@ -103,7 +103,7 @@ def main():
     job_title = st.text_input("🔧 Stichwort (z. B. Python Entwickler)", "python")
     location = st.text_input("📍 Ort (z. B. Berlin)", "Berlin")
     results_per_page = st.slider("📄 Anzahl der Ergebnisse", min_value=1, max_value=20, value=5)
-
+    url = f'https://api.adzuna.com/v1/jobs/ch/search/1'
     # Button zum Auslösen der Suche
     if st.button("🔎 Jobs suchen"):
         with st.spinner("Suche läuft..."):
@@ -116,7 +116,7 @@ def main():
                 'content-type': 'application/json'
             }
 
-            response = requests.get(BASE_URL, params=params)
+            response = requests.get(url, params=params)
 
             if response.status_code == 200:
                 data = response.json()
